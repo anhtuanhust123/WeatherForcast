68289c2c24d9e63101f4443fef2350038b0671f4
asus <ngocanhtuan1472001@gmail.com>
2022-08-19 16:25:17 +0700

    Initial commit

---
+++ app/src/main/java/com/vnpt/weatherforcast/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.vnpt.weatherforcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+    TextView appName;
+    LottieAnimationView lottieAnimationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        appName=findViewById(R.id.app_name);
+        lottieAnimationView=findViewById(R.id.animationView);
+        appName.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        lottieAnimationView.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },5000);
+
+    }
+}
